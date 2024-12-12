c2154f9259556865d38eb20b00964037a6f6186d
S.A <madjuggler1216@gmail.com>
2024-12-12 21:39:54 +0900

    commit

--- VBHandsOn/Ex11/Employee.vb
+++ VBHandsOn/Ex11/Employee.vb
@@ -1,3 +1,52 @@
 ﻿Public Class Employee
+    'フィールド定義
+    Private lastnameValue As String
+    Private firstnameValue As String
+    Private age As Integer
+    Public Shared companyName As String = "ビプ商事"
 
+    'プロパティ定義
+    Public Property Lastname() As String
+        Get
+            Return lastnameValue
+        End Get
+        Set(value As String)
+            If Not String.IsNullOrEmpty(value) Then
+                lastnameValue = value
+            End If
+        End Set
+    End Property
+    Public ReadOnly Property Firstname() As String
+        Get
+            Return firstnameValue
+        End Get
+    End Property
+
+    'コンストラクター定義
+    Public Sub New()
+        lastnameValue = "姓なし"
+        firstnameValue = "名なし"
+        age = 20
+    End Sub
+    Public Sub New(lname As String, fname As String, n As Integer)
+        lastnameValue = lname
+        firstnameValue = fname
+        age = n
+    End Sub
+
+    'メソッド定義
+    Public Function GetFullName() As String
+        Return Lastname & Firstname
+    End Function
+    Public Function GetFullName(delim As String) As String
+        Return Lastname & delim & Firstname
+    End Function
+    Public Sub PrintInfo()
+        Console.WriteLine("姓名：" & Lastname & Firstname)
+        Console.WriteLine("年齢：" & age)
+    End Sub
+
+    Shared Sub ClassInfo()
+        Console.WriteLine("Employeeクラスです")
+    End Sub
 End Class
--- VBHandsOn/Ex11/Program.vb
+++ VBHandsOn/Ex11/Program.vb
@@ -2,6 +2,28 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("会社名：" & Employee.companyName)
+        Employee.ClassInfo()
+        Console.WriteLine()
+
+        Dim emp1 As Employee = New Employee()
+        emp1.PrintInfo()
+        Console.Writeline()
+        Dim emp2 As Employee = New Employee("豊洲", "太郎", 28)
+        emp2.PrintInfo()
+        Console.Writeline("フルネームは" & emp2.GetFullName() & "さんです")
+
+        Console.WriteLine()
+        Console.Writeline("オーバーロードしたメソッドを呼び出します")
+        Console.Writeline("フルネームは" & emp2.GetFullName("　") & "さんです")
+
+        Console.Writeline()
+        Console.Writeline("プロパティを使用して値の取得、設定を行います")
+        Console.Writeline("変更前のフルネームは" & emp2.Lastname & emp2.Firstname & "です")
+        emp2.Lastname = "川崎"
+        Console.Writeline("変更後のフルネームは" & emp2.Lastname & emp2.Firstname & "です")
+
+        Console.Readline()
     End Sub
 End Module
+
